a3baf8eaa4eba65419bd26c4413cfa022a363c13
Sachin Kumaran <88617925+Sachin-Kumaran@users.noreply.github.com>
2025-11-12 17:37:15 +0530

    Update wanhai.py

--- wanhai.py
+++ wanhai.py
@@ -4,7 +4,7 @@ def scrape_wanhai():
     URL = "https://www.wanhai.com/views/skd/SkdBySvc.xhtml?file_num=64836&parent_id=64834&top_file_num=64735"
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
             viewport={"width":1280, "height":800},
