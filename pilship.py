15c42378a8a85bad6d966933f5d84e1cb251c470
Sachin Kumaran <88617925+Sachin-Kumaran@users.noreply.github.com>
2025-11-13 11:26:38 +0530

    Update pilship.py
    This updated code , works for the point to point schedules in pilship works by saving a csv file as the output

--- pilship.py
+++ pilship.py
@@ -1,23 +1,43 @@
-import requests
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+from io import StringIO
 
-url = "https://www.pilship.com/wp-content/themes/hello-theme-child-master/pil-api/schedules-byport.php?port_code=INMAA&selected_portname=%5BINMAA%5D%20CHENNAI&byport_selectedstartdate=2025-11-10&byport_selectedenddate=2025-11-29&n=1762765203%7C6248f694b6e90a8e3dddcfe905c7bf41&timestamp=1762765717881"
+url = "https://www.pilship.com/wp-content/themes/hello-theme-child-master/pil-api/schedules-point2point.php?port_code_origin=JPTYO&port_code_destination=INMAA&p2p_selectedstartdate=2025-10-01&p2p_selectedenddate=2025-11-29&n=1763010003%7Ca89d3db293a0021da1c0ccd94077ae19&timestamp=1763010319864"
 
-payload = {}
 headers = {
-  'Accept': '*/*',
-  'Sec-Fetch-Site': 'same-origin',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Sec-Fetch-Mode': 'cors',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Safari/605.1.15',
-  'Referer': 'https://www.pilship.com/digital-solutions/?tab=customer&id=schedules&label=byPort&port-code=INMAA&start-date=2025-11-10&end-date=2025-11-29',
-  'Connection': 'keep-alive',
-  'Cookie': '_ga_M39J8YZNDE=GS2.1.s1762765546$o1$g1$t1762765723$j23$l0$h0; _ga=GA1.1.991735325.1762765547; OClmoOot=Ay4HBG2aAQAADDJ4PR8NFTpAFlml8_Hfi4MGCxYjGnuQxXEgHUCxJp9hcrrhAQ5g7U76K-mbwH9eCOfvosJeCA|1|0|dc45a1d1ad4b7674677a923aff48ed658a540806; wp-wpml_current_language=en; CookieConsent={stamp:%27DtM1GmBGpvW6cjSVe2jU0rn9WnfW4Gz5tNWvwT4046+EL8vZvlTwyA==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1762765544473%2Cregion:%27in%27}',
-  'Sec-Fetch-Dest': 'empty',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Priority': 'u=3, i'
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Cookie": "_ga=GA1.1.991735325.1762765547; _ga_M39J8YZNDE=GS2.1.s1763009634$o3$g1$t1763010318$j59$l0$h0; OClmoOot=Ay4HBG2aAQAADDJ4PR8NFTpAFlml8_Hfi4MGCxYjGnuQxXEgHUCxJp9hcrrhAQ5g7U76K-mbwH9eCOfvosJeCA|1|0|dc45a1d1ad4b7674677a923aff48ed658a540806; wp-wpml_current_language=en; CookieConsent={stamp:'DtM1GmBGpvW6cjSVe2jU0rn9WnfW4Gz5tNWvwT4046+EL8vZvlTwyA==',necessary:true,preferences:true,statistics:true,marketing:true,method:'explicit',ver:1,utc:1762765544473,region:'in'}",
+    "Priority": "u=3, i",
+    "Referer": "https://www.pilship.com/digital-solutions/?tab=customer&id=schedules&label=pointToPoint&origin-port-code=JPTYO&destination-port-code=INMAA&start-date=2025-10-01&end-date=2025-11-29",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.1 Safari/605.1.15",
+    "X-Requested-With": "XMLHttpRequest"
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    response = page.request.get(url, headers=headers)
+    data = json.loads(response.text())
+    browser.close()
 
-print(response.text)
+html_content = data.get("schedulePointToPoint", {}).get("main", "")
+soup = BeautifulSoup(html_content, "html.parser")
+table = soup.find("table")
+
+if table:
+    df = pd.read_html(StringIO(str(table)))[0]
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+    if "Port of Load" in df.columns:
+        df = df.dropna(subset=["Port of Load"])
+    df.to_csv("table_p2p.csv", index=False, encoding="utf-8-sig")
+    print("Cleaned table saved as 'table_p2p.csv'")
+else:
+    print("No table found in the JSON 'main' field.")
